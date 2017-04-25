03addf46922b7db73ad7e7a98c64b3eafaac65ea
pjhl <pjhl.coder@gmail.com>
2017-04-25 17:17:53 +0300

    Try direct password

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ chkconfig squid on
 # Copy config from github
 curl -o /etc/squid/squid.conf https://raw.githubusercontent.com/pjhl/centos7-proxy/master/config/squid.conf
 # Add user
-htpasswd -bc /etc/squid/.htpasswd user $PROXY_PASS
+htpasswd -bc /etc/squid/.htpasswd user 1111
 systemctl start squid
